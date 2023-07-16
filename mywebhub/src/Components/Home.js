7d378576e181c1f5f9dac441e39a3014671648e5
ashutosh <ashutosh9907@gmail.com>
2023-07-16 09:59:28 +0530

    home page

--- mywebhub/src/Components/Home.js
+++ mywebhub/src/Components/Home.js
@@ -1,5 +1,10 @@
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <h1 className="font-bold text-xl">Home</h1>
+      <h2 className="text-xl">This is home Page</h2>
+    </>
+  );
 };
 
 export default Home;
