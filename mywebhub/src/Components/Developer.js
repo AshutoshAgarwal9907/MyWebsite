764409f20ac043aef5f7032b4f58680a588eb1aa
ayush agrawal <ayushagrawal2525@gmail.com>
2023-07-16 18:00:00 +0530

    change for page

--- mywebhub/src/Components/Developer.js
+++ mywebhub/src/Components/Developer.js
@@ -1,2 +1,15 @@
-function Developer() {}
+function Developer() 
+{
+    return(
+    <div>
+    <div class="explore-banner leetcodes-interview-crash-course-data-structures-and-algorithms">
+    <div class="background" style={{ 
+      backgroundImage: `url("https://assets.leetcode.com/explore/cards/leetcodes-interview-crash-course-data-structures-and-algorithms/banner-1663092678.png" 
+      background-size: auto 100%; background-position: right center; background-repeat: no-repeat;)` 
+    }}>; 
+   </div>
+   </div> 
+   </div>
+    )
+}
 export default Developer;
--- mywebhub/src/Components/Product.js
+++ mywebhub/src/Components/Product.js
@@ -1,4 +1,18 @@
 function Product() {
-  return <div>product</div>;
+  return(
+    
+    <div class="explore-banner leetcodes-interview-crash-course-data-structures-and-algorithms">
+    <div class="background" style={{ 
+      backgroundImage: `url("https://assets.leetcode.com/explore/cards/leetcodes-interview-crash-course-data-structures-and-algorithms/banner-1663092678.png" 
+      background-size: auto 100%; background-position: right center; background-repeat: no-repeat;)` 
+    }}>; 
+    <div class="container"  style={{ 
+      backgroundImage: `url("https://assets.leetcode.com/explore/cards/leetcodes-interview-crash-course-data-structures-and-algorithms/banner-1663092678.png" 
+      background-size: auto 100%; background-position: right center; background-repeat: no-repeat;)` 
+    }}>; 
+   </div>
+   </div> 
+   </div>
+    )
 }
 export default Product;
